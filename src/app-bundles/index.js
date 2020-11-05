1c13fadeb61e584c6a5ed5419f5d7608a9647d75
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2020-11-05 15:10:47 -0600

    fix logout redirect

--- src/app-bundles/index.js
+++ src/app-bundles/index.js
@@ -5,7 +5,7 @@ import {
 } from "redux-bundler";
 
 import createNestedUrlBundle from "./create-nested-url-bundle";
-import createAuthBundle from "@corpsmap/create-auth-bundle";
+import createAuthBundle from "./create-auth-bundle";
 import pkg from "../../package.json";
 
 import routeBundle from "./routes-bundle";
@@ -15,17 +15,8 @@ import cache from "./../cache.js";
 export default composeBundles(
   createAuthBundle({
     appId: "ff3437e4-f2fc-432f-8175-7dd70f9bda44",
-    redirectOnLogout: "/",
+    redirectOnLogout: pkg.homepage,
   }),
-  // createJwtApiBundle({
-  //   root:
-  //     process.env.NODE_ENV === "development"
-  //       ? `http://api.rsgis.dev/development`
-  //       : `https://api.rsgis.dev/development`,
-  //   unless: {
-  //     method: "GET",
-  //   },
-  // }),
   createCacheBundle({
     cacheFn: cache.set,
   }),
